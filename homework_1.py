9ce693fb5e98a017bf5f171a21d4091c859c840c
Pavlo <pavlikbartmanskiy@gmail.com>
2026-03-17 22:36:32 +0200

    homework_1 version 2.0

--- homework_1.py
+++ homework_1.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    return (datetime.today().date() - datetime.strptime(date, "%Y.%m.%d").date()).days
+    try:
+        return (datetime.today().date() - datetime.strptime(date, "%Y-%m-%d").date()).days
+    except ValueError:
+        return 'time data does not match right format' 
 
-user_date = "2026.04.01"
+
+user_date = "2016-04-03"
 print(get_days_from_today(user_date)) 
